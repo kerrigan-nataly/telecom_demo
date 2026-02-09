a938b42cb54a1aff85fbb3d6ef2ab5601408a849
Kabachok <94787801+kerrigan-nataly@users.noreply.github.com>
2026-02-09 14:06:56 +0300

    init
    only the styles and the elements so far. no interface or navi or even packages. we are gettin there first i need a db

---
+++ telecom_demo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace telecom_demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
